58e315a2eec55ff231b6b9990bfff6aab3a1927e
ronaldochoiga <choigaronaldo@gmail.com>
2023-06-27 10:56:53 -0700

    task 2 code fail

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,5 +7,6 @@ class Square:
     """Initialises the data found in the square
     """
     def __init__(self, size):
-
+        """creates one instace of a square
+        """
         self.__size = size
