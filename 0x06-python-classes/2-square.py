23e0bc7c6b748254c2cbba4e3c482b79e5f59bb2
ronaldochoiga <choigaronaldo@gmail.com>
2023-06-27 11:19:33 -0700

    task 3 code fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,15 +3,19 @@
 """Defines a square in the python code"""
 
 class Square:
+
     """ class that Defines a square in the python code
     Attributes:
     Size: 1 sided square
     """
+    
     def __init__(self, size=0):
+
         """Initialises the data inside the class
         Args:
         Size: one sided quare
         """
+        
         self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
