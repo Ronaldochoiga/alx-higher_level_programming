51fe434051c1cbc29cbcb09f830a8bcb1a5df6d3
ronaldochoiga <choigaronaldo@gmail.com>
2023-06-27 11:05:31 -0700

    task 2 code fail

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
 
-"""Defines the square in question"""
 
 class Square:
-   """
-    Class that defines properties of square by:
+    """
+    Class that defines properties of square
     Attributes:
         size: size of a square (1 side).
     """
-    def __init__(self, size): 
-       """Creates new instances of square (1 side).
+    def __init__(self, size):
+        """Creates new instances of square
         Args:
             size: size of the square.
         """
-       self.__size = size
+        self.__size = size
