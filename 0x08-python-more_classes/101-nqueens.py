2d562dd68f3a8f5492643fc13630c09f9a836ff9
ronaldochoiga <choigaronaldo@gmail.com>
2023-07-16 10:42:45 -0700

    task 10

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,63 +1,57 @@
 #!/usr/bin/python3
-"""N Queens Module.
-
-Contains the N Queens problem solver.
 """
-import sys
+nqueens backtracking program to print the coordinates of n queens
+on an nxn grid such that they are all in non-attacking positions
+"""
+from sys import argv
+if __name__ == "__main__":
+    c = []
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if argv[1].isdigit() is False:
+        print("N must be c number")
+        exit(1)
+    n = int(argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+    # initialize the answer list
+    for k in range(n):
+        c.append([k, None])
 
+    def already_exists(q):
+        """check that c queen does not exist in that q value"""
+        for p in range(n):
+            if q == c[p][1]:
+                return True
+        return False
 
-def error_exit(message="", code=1):
-    """Handles exit.
-
-    Args:
-        message (str): the message to display on stdout.
-        code (int): the exit code.
-    """
-    print(message)
-    exit(code)
-
-
-def test_pos(board, a):
-    """Tests if queen can be placed at the current pos.
-
-    Args:
-        board (list): the chessboard.
-        a (int): the height par.
-    """
-    for i in range(a):
-        if board[a][1] is board[i][1]:
+    def reject(p, q):
+        """determines whether to reject the solution or not"""
+        if (already_exists(q)):
             return False
-        if abs(board[a][1] - board[i][1]) == a - i:
-            return False
-    return True
+        k = 0
+        while(k < p):
+            if abs(c[k][1] - q) == abs(k - p):
+                return False
+            k += 1
+        return True
 
+    def clear_a(p):
+        """clears the ans from the point of failure onwards"""
+        for k in range(p, n):
+            c[k][1] = None
 
-def rec_backtrack(board, a):
-    """track the possibility.
-
-    Args:
-        board (list): the chessboard.
-        a (int): the height parameter.
-    """
-    if a is N:
-        print(board)
-    else:
-        for b in range(N):
-            board[a][1] = b
-            if test_pos(board, a):
-                rec_backtrack(board, a + 1)
-
-
-if len(sys.argv) is not 2:
-    error_exit("Usage: nqueens N")
-
-try:
-    N = int(sys.argv[1])
-except:
-    error_exit("N must be a number")
-
-if N < 4:
-    error_exit("N must be at least 4")
-
-board = [[a, 0] for a in range(N)]
-rec_backtrack(board, 0)
+    def nqueens(p):
+        """recursive backtracking function to find the sol"""
+        for q in range(n):
+            clear_a(p)
+            if reject(p, q):
+                c[p][1] = q
+                if (p == n - 1):  # accepts the solution
+                    print(c)
+                else:
+                    nqueens(p + 1)  # moves on to next p value to continue
+    # start the recursive process at p = 0
+    nqueens(0)
