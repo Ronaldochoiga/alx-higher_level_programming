e78e79e254c5ed225c596ef1cae1fc0b68ba93b0
ronaldochoiga <choigaronaldo@gmail.com>
2023-09-30 14:01:08 -0700

    task 1 and 2 added

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script shows the size of url request.
-#the script uses curl command.
+# This script shows the size of url request. The script uses curl command.
 curl -s -w %{size_download}"\n" "$1" -o /dev/null
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a GET request.
-# The script utilizes a url to send another url request through curl.
+# This script sends a GET request. The script utilizes a url to send another url request through curl.
 curl -sL "$1"
